ef0a62de3458a9a94b855ff2655c7a5c1320e19d
dylan <birds@DylanW-PC>
2024-09-08 02:49:34 +1000

    backend/air-pollution

---
+++ backend/data/data_air_pollution.py
@@ -0,0 +1,37 @@
+import requests
+import pandas as pd
+import os
+import json
+from io import StringIO
+
+## Climate Data Cube Yearly Slices of Observations
+sites = []
+sites_dict = {}
+url = "https://data.airquality.nsw.gov.au/api/Data/get_SiteDetails"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()  # Parse JSON response
+    for site in data:
+        if site['Site_Id'] not in sites:
+            sites.append(site['Site_Id'])
+            sites_dict[site['Site_Id']] = site['SiteName']
+else:
+    print(f"Error: {response.status_code}")
+
+payload = {
+    "Parameters": ["PM10", "PM2.5"],
+    "Sites": sites,
+    "StartDate": "2024-09-06",
+    "EndDate": "2024-09-07",
+    "Categories": ["Averages"],
+    "SubCategories": ["Hourly", "Daily"],
+    "Frequency": ["24h rolling average derived from 1h average"]
+}
+payload_json = json.dumps(payload)
+url = 'https://data.airquality.nsw.gov.au/api/Data/get_Observations'
+response = requests.post(url, json=payload_json)
+
+df = pd.read_json(StringIO(response.text))
+df['siteName'] = df.apply(lambda row:  sites_dict[row['Site_Id']], axis=1)
+df.to_csv('backend/data/datasets/nsw_air_pollution.csv', index=False)
+
